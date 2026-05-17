d2b6fdbdc77c08bb3d8485f6afd817dd6b782759
Sushant Kumar <sk87896821@gmail.com>
2026-05-17 16:13:32 +0530

    Fix typo in debug flag for app.run

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def upload_file():
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(host='0.0.0.0,  port=5001, debug=flase)
+    app.run(host='0.0.0.0,  port=5001, debug=False)
