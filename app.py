3acad62b51c6842d555389c68bf04020c57617bd
Sushant Kumar <sk87896821@gmail.com>
2026-05-17 16:03:00 +0530

    Change app.run to listen on all interfaces

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def upload_file():
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0,  port=5001, debug=True)
